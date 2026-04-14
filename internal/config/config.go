37862919575475f4ea64bed9f57d2ed8edba20bd
Karma-234 <busari1kamal@gmail.com>
2026-04-14 18:13:01 +0100

    Add: config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"os"
+
+	"go.yaml.in/yaml/v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"server"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
