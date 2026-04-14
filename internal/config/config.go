b39f6f6908f297f1dd03a3ef91336c36a2d20ae1
Karma-234 <busari1kamal@gmail.com>
2026-04-14 18:30:25 +0100

    Update: config value and struct fields

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,6 +11,22 @@ type Config struct {
 		Host string `yaml:"host"`
 		Port int    `yaml:"port"`
 	} `yaml:"server"`
+	Agents []Agent `yaml:"agents"`
+	Debate struct {
+		MaxRounds          int  `yaml:"max_rounds"`
+		StrictUnanimity    bool `yaml:"strict_unanimity"`
+		FallbackOnDeadlock bool `yaml:"fallback_on_deadlock"`
+	} `yaml:"debate"`
+	Output struct {
+		DefaultMode string `yaml:"default_mode"`
+	} `yaml:"output"`
+}
+
+type Agent struct {
+	Name     string `yaml:"name"`
+	Role     string `yaml:"role"`
+	Model    string `yaml:"model"`
+	Provider string `yaml:"provider"`
 }
 
 func LoadConfig(path string) (*Config, error) {
